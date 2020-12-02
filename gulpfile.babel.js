6ad34b00d3df84c1cd1db9f8c8a811a82cd2c5ac
LJ <jhlee9193@gmail.com>
2020-12-02 09:17:22 +0900

    Gulp Sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import gimage from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
@@ -13,6 +16,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img",
+  },
+  sass: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
   },
 };
 
@@ -26,10 +34,20 @@ const webserver = () =>
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, style);
 };
 
-const img = () =>
+const img = () => {
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(route.img.dest));
+};
+
+const style = () => {
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+};
 
 const prepare = gulp.dest([clean, img]);
 const assets = gulp.series([pug]);
